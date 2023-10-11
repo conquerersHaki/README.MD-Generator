ec91a53ee5031ea1994f932ff4aff544f155ab42
unknown <keighleychristina@yahoo.com>
2023-10-10 21:22:03 -0700

    completed index.js

--- develop/index.js
+++ develop/index.js
@@ -1,111 +1,141 @@
 //packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
-const util = require('util');
-
-// modules needed for this application
-const generateMarkdown = require('./utils/generateMarkdown.js');
+const { message } = require('statuses');
 
 
 //array of questions for user input
-const questions = [
-
-    {
-        type: 'input',
-        message: "What is your GitHub username?",
-        name: 'username',
-    },
+inquirer.prompt (
+    [
+        
+        {
+            type: 'input',
+            message: "What is the title of your project?",
+            name: 'title',
+            validate: (value) => {if(value){return true} else {return 'Please enter required info to continue.'}},
+        },
 
         
-    {
-        type: 'input',
-        message: "What is the name of your GitHub repository?",
-        name: 'repo',
-    },
+        {
+            type: 'input',
+            message: "Provide a description of your project.",
+            name: 'description',
+            validate: (value) => {if(value){return true} else {return 'Please enter required info to continue.'}},
+        },
 
+        
+        {
+            type: 'input',
+            message: "Explain the steps required to install your project.",
+            name: 'installation',
+            validate: (value) => {if(value){return true} else {return 'Please enter required info to continue.'}},
+        },
+
+        
+        {
+            type: 'input',
+            message: "Give instructions and examples of your project.",
+            name: 'usage',
+            validate: (value) => {if(value){return true} else {return 'Please enter required info to continue.'}},
+        },
+
+        {
+            type: 'input',
+            message: "Offer guidelines on how other developers can contribute to your project code.",
+            name: 'contributiion',
+            validate: (value) => {if(value){return true} else {return 'Please enter required info to continue.'}},
+        },
+
+        
+        {
+            type: 'input',
+            message: "Explain any tests for your project and examples of how ot run them.",
+            name: 'tests',
+            validate: (value) => {if(value){return true} else {return 'Please enter required info to continue.'}},
+        },
+
+        {
+            type: 'list',
+            message: "Select the license you would like to use.",
+            choices: ['GNU AGPLv3', 'GNU GPLv3', 'GNU LGPLv3', 'Mozilla Public License 2.0', 'Apache License 2.0', 'MIT License', 'Boost Software License 1.0', 'The Unlicense'],
+            name: 'license',
+            validate: (value) => {if(value){return true} else {return 'Please enter required info to continue.'}},
+        },
+
+        {
+            type: 'input',
+            message: "Enter your email in case anyone would like to reach you reagarding this project.",
+            name: 'email',
+            validate: (value) => {if(value){return true} else {return 'Please enter required info to continue.'}},
+        },
+
+        {
+            type: 'input',
+            message: "What is your GitHub username?",
+            name: 'username',
+            validate: (value) => {if(value){return true} else {return 'Please enter required info to continue.'}},
+        },
+
+    ]
+).then(( {
+    title,
+    description,
+    installation,
+    usage,
+    contribution,
+    tests,
+    license,
+    email,
+    username
+
+// template for user input
+}) => {
+    const template = `## ${title}
     
-    {
-        type: 'input',
-        message: "What is the title of your project?",
-        name: 'project title',
-    },
 
+    * [Description](#description)
+    * [Installation](#installation)
+    * [Usage](#usage)
+    * [Contribution](#contribution)
+    * [Tests](#tests)
+    * [License](#License)
     
-    {
-        type: 'input',
-        message: "Provide a description of your project.",
-        name: 'description',
-    },
+    ## Description
+    ${Description}
 
-    
-    {
-        type: 'input',
-        message: "Explain the steps required to install your project.",
-        name: 'installation',
-    },
+    ## Installation
+    ${installation}
 
-    
-    {
-        type: 'input',
-        message: "Give instructions and examples of your project.",
-        name: 'usage',
-    },
+    ## Usage
+    ${usage}
 
-    {
-        type: 'input',
-        message: "Offer guidelines on how other developers can contribute to your project code.",
-        name: 'contributing'
-    },
+    ## Contribution
+    ${contribution}
 
-    
-    {
-        type: 'input',
-        message: "Explain any tests for your project and examples of how ot run them.",
-        name: 'tests',
-    },
+    ## Tests
+    ${tests}
 
-    {
-        type: 'list',
-        message: "Select the license you would like to use.",
-        choices: ['GNU AGPLv3', 'GNU GPLv3', 'GNU LGPLv3', 'Mozilla Public License 2.0', 'Apache License 2.0', 'MIT License', 'Boost Software License 1.0', 'The Unlicense'],
-        name: 'license',
-    },
+    ## License
+    ${license}
 
+    # Contact
+    GitHub: ${username}
+    Email: ${email}`;
 
-];
-
-//function to write README file
-function writeToFile(fileName, data) {
-    fs.writeFile(fileName, data, err => {
-
-        if (err) {
-            return console.log(err);
-        }
-
-        console.log("All done! Your README.md file has populated!");
-    });
+    // to create the readme file
+    createNewFile(title, template);
 }
+);
 
-//async function to initialize app
-async function init() {
+// function for creating the readme file
 
-    try {
-
-        const userResponse = await inquirer.prompt(questions);
-        console.log("Your response: ", userResponse);
-        console.log("Thank you for your input!")
-
-        console.log("Generating your README.md file...");
-        const markdown = generateMarkdown(userResponse);
-        console.log(markdown);
-
-        await writeFile('README.md', markdown);
-
-    } catch (error) {
-
-        console.log(error);
-    }
+function createNewFile(fileName, template) {
+    // adding the file, changing it to lowercase to be compatible. if theres an error, log it
+    fs.writeFile(`./${fileName.toLowerCase().split(' ').join('')}.md`, data, (err) => {
+        if(err) {
+            console.log(err)
+        }
+        // we did it kids
+        console.log('All done! Your README.MD file has been generated!');
+    });
 };
-
-// Function call to initialize app
-init();
